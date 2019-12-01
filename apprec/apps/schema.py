f3ecf74324b2bee895085b18f82e04c73b6fbfeb
davidcechak <cechyy@gmail.com>
2019-12-01 15:39:04 +0100

    dummy recommendedApps field

--- apprec/apps/schema.py
+++ apprec/apps/schema.py
@@ -1,17 +1,29 @@
 import graphene
 from graphene_django import DjangoObjectType
 from django.db.models.query import QuerySet
+from django.db.models import Q
 
 from .models import App
 
 
 class AppType(DjangoObjectType):
+    recommendedApps = graphene.List(lambda: AppType)
     class Meta:
         model = App
-
+    def resolve_recommendedApps(self, info):
+        #ToDo call some recommenderSys algo to return recommended apps
+        #object 'self' is current App
+        return [self, self, self, self]
 
 class Query(graphene.ObjectType):
-    apps = graphene.List(AppType)
+    apps = graphene.List(AppType, id=graphene.String())
 
-    def resolve_apps(self, info, **kwargs):
+    def resolve_apps(self, info, id=None, **kwargs):
+        # ToDo: extend this if it is necessary to filter using more/other attributes
+        if id:
+            filterQuery = (
+                Q(id=id)
+            )
+            return App.objects.using('default').filter(filterQuery)
         return App.objects.using('default')
+
